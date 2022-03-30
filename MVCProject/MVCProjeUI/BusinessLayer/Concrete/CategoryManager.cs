dd6c930d16ed5008d9a4cb95c8fa0d1dfcc9ad08
KaramanTugba <97016474+KaramanTugba@users.noreply.github.com>
2022-03-30 10:38:18 +0300

    28.03.2022

--- MVCProject/MVCProjeUI/BusinessLayer/Concrete/CategoryManager.cs
+++ MVCProject/MVCProjeUI/BusinessLayer/Concrete/CategoryManager.cs
@@ -1,4 +1,6 @@
-﻿using DataAccessLayer.Concrete.Repositories;
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete.Repositories;
 using EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
@@ -8,21 +10,32 @@ using System.Threading.Tasks;
 
 namespace BusinessLayer.Concrete
 {
-    public class CategoryManager
+    public class CategoryManager:ICategoryService
     {
-        GenericRepository<Category> repo = new GenericRepository<Category>();
+        ICategoryDAL _categoryDal;
 
-        public List<Category> GetAll()
+        public void CategoryAddBL(Category category)
         {
-            return repo.List();
+            _categoryDal.Insert(category); 
         }
-        public void CategoryAddBL(Category p)
+
+        public List<Category> GetList()
         {
-            if (p.CategoryName == "" || p.CategoryName.Length < 3 || p.CategoryDescription == "")
-            {
-                //hata mesajı
-            }
-            else { repo.Insert(p); }
+            return _categoryDal.List();
         }
+        //GenericRepository<Category> repo = new GenericRepository<Category>();
+
+        //public List<Category> GetAll()
+        //{
+        //    return repo.List();
+        //}
+        //public void CategoryAddBL(Category p)
+        //{
+        //    if (p.CategoryName == "" || p.CategoryName.Length < 3 || p.CategoryDescription == "")
+        //    {
+        //        //hata mesajı
+        //    }
+        //    else { repo.Insert(p); }
+        //}
     }
 }
