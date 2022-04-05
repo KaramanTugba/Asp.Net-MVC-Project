70785a0d48bb384a566191499eb57e5d644c0237
KaramanTugba <97016474+KaramanTugba@users.noreply.github.com>
2022-04-05 11:29:48 +0300

    added NoNeed.txt

---
+++ ECommerceLiteProject/ECommerceLiteBLL/Settings/SiteSettings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceLiteBLL.Settings
+{
+   public class SiteSettings
+    {
+        // ToDo: Mail adresini webconfig dosyasından çekmeyi dde öğrenelim.
+
+        public static string SiteMail { get; set; } = "nayazilim303@gmail.com";
+        public static string SiteMailPassword { get; set; } = "betul303303";
+        public static string SiteMailSmtpHost { get; set; } = "smtp.gmail.com";
+        public static string SiteMailSmtpPost { get; set; } = "587";
+        public static bool SiteMailEnableSSL = true;
+
+        //buraya geri döneceğiz
+
+
+    }
+}
