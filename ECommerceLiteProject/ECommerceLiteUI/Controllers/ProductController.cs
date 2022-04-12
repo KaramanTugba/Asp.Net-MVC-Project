af7cb5f56dedfe76a59f9c090db2b2be4958dbed
KaramanTugba <97016474+KaramanTugba@users.noreply.github.com>
2022-04-12 14:17:57 +0300

    added: Logo,ProductController

---
+++ ECommerceLiteProject/ECommerceLiteUI/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ECommerceLiteUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        //Bu controller a admin gibi yetkili kişiler erişebilecektir
+        //burada ürünlerin listenmesi ekleme,silme, güncelleme işlemleri yapılacaktır.
+        public ActionResult ProductList()
+        {
+            return View();
+        }
+
+    }
+}
