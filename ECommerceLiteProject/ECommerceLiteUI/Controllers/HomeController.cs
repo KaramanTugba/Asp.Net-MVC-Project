f4fcaaedd8994a449fcc69dd68ccd612b994fd6b
KaramanTugba <97016474+KaramanTugba@users.noreply.github.com>
2022-04-19 15:03:50 +0300

    added Products in Index

--- ECommerceLiteProject/ECommerceLiteUI/Controllers/HomeController.cs
+++ ECommerceLiteProject/ECommerceLiteUI/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ECommerceLiteBLL.Repository;
+using ECommerceLiteUI.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +10,38 @@ namespace ECommerceLiteUI.Controllers
 {
     public class HomeController : Controller
     {
+        CategoryRepo myCategoryRepo = new CategoryRepo();
+        ProductRepo myProductRepo = new ProductRepo();
         public ActionResult Index()
         {
-            return View();
+            // ANA KATEGORİLERİ VİEWBAG İLE SAYFAYA GÖNDERELİM
+
+            var categoryList = myCategoryRepo.AsQueryable().Where(x => x.BaseCategoryId == null).Take(4).ToList();
+            ViewBag.CategoryList = categoryList.OrderByDescending(x => x.Id);
+            //
+            var productList = myProductRepo.AsQueryable().Where(x => !x.IsDeleted && x.Quantity >= 1).Take(10).ToList();
+            List<ProductViewModel> model = new List<ProductViewModel>();
+            foreach (var item in productList)
+            {
+                var product = new ProductViewModel()
+                {
+                    Id = item.Id,
+                    CategoryId=item.CategoryId,
+                    ProductName=item.ProductName,
+                    Description=item.Description,
+                    Quantity=item.Quantity,
+                    Discount=item.Discount,
+                    RegisterDate=item.RegisterDate,
+                    Price=item.Price,
+                    ProductCode=item.ProductCode
+                    //isdeleted alanı view modelin içerisine eklemedik
+                };
+                product.GetCategory();
+                product.GetProductPictures();
+                model.Add(product);
+               
+            }
+            return View(model);
         }
 
         public ActionResult About()
