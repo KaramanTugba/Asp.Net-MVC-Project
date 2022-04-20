4988c321643343c4b97feda772968272b4a95366
KaramanTugba <97016474+KaramanTugba@users.noreply.github.com>
2022-04-20 14:13:25 +0300

    Bugs fixed
    1- GetCategory bugs fixed in ProductViewModel
    2- Product prop. changed for Mapster
    3- Webp format error fixed

--- ECommerceLiteProject/ECommerceLiteUI/Controllers/HomeController.cs
+++ ECommerceLiteProject/ECommerceLiteUI/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using ECommerceLiteBLL.Repository;
 using ECommerceLiteUI.Models;
+using Mapster;
 
 namespace ECommerceLiteUI.Controllers
 {
@@ -25,29 +26,35 @@ namespace ECommerceLiteUI.Controllers
             var productList = myProductRepo.AsQueryable()
                 .Where(x => !x.IsDeleted && x.Quantity >= 1).Take(10).ToList();
             List<ProductViewModel> model = new List<ProductViewModel>();
-            foreach (var item in productList)
-            {
-                //mapster :S 
-                //model.Add(item.Adapt<ProductViewModel>());
-                var product = new ProductViewModel()
-                {
-                    Id = item.Id,
-                    CategoryId = item.CategoryId,
-                    ProductName = item.ProductName,
-                    Description = item.Description,
-                    Quantity = item.Quantity,
-                    Discount = item.Discount,
-                    RegisterDate = item.RegisterDate,
-                    Price = item.Price,
-                    ProductCode = item.ProductCode
-                    //isDeleted alanını viewmodelin içine eklemeyi unuttuk. Çünkü
-                    // isDeleted alanını daha dün ekledik. Viewmodeli geçen hafta oluşturduk
-                };
-                product.GetCategory();
-                product.GetProductPictures();
-                model.Add(product);
-            }
 
+            //foreach (var item in productList)
+            //{
+
+            //    var product = new ProductViewModel()
+            //    {
+            //        Id = item.Id,
+            //        CategoryId = item.CategoryId,
+            //        ProductName = item.ProductName,
+            //        Description = item.Description,
+            //        Quantity = item.Quantity,
+            //        Discount = item.Discount,
+            //        RegisterDate = item.RegisterDate,
+            //        Price = item.Price,
+            //        ProductCode = item.ProductCode
+            //        //isDeleted alanını viewmodelin içine eklemeyi unuttuk. Çünkü
+            //        // isDeleted alanını daha dün ekledik. Viewmodeli geçen hafta oluşturduk
+            //    };
+            //    product.GetCategory();
+            //    product.GetProductPictures();
+            //    model.Add(product);
+            //}
+            //Mapster ile mapledik
+            productList.ForEach(x => {
+                var item = x.Adapt<ProductViewModel>();
+                item.GetCategory();
+                item.GetProductPictures();
+                model.Add(item);
+            });
             return View(model);
         }
 
@@ -63,6 +70,73 @@ namespace ECommerceLiteUI.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+        public ActionResult AddToCard(int id)
+        {
+            try
+            {
+                //session eklenecek
+                //session oturum demektir
+                var shoppingCard = Session["ShoppingCard"] as List<ProductViewModel>;
+
+                if (shoppingCard==null)
+                {
+                    shoppingCard = new List<ProductViewModel>();
+                }
+                if (id>0)
+                {
+                    var product = myProductRepo.GetById(id);
+                    if (product==null)
+                    {
+                        //ex loglanacak. product null geldi.
+                        TempData["AddToCardFailed"] = "ürün eklemesi başarısız oldu.Lütfen tekrar deneyiniz";
+                        return RedirectToAction("Index", "Home");
+                    }
+                    //tamam ekleme yapılacak.
+                    //ProductViewModel productAddToCard=product.Adapt<ProductViewModel>();
+                    var productAddToCard = new ProductViewModel()
+                    {
+                        Id = product.Id,
+                        ProductName = product.ProductName,
+                        Description = product.Description,
+                        CategoryId = product.CategoryId,
+                        Discount = product.Discount,
+                        Price = product.Price,
+                        Quantity = product.Quantity,
+                        RegisterDate = product.RegisterDate,
+                        ProductCode = product.ProductCode
+                    };
+                    if (shoppingCard.Count(x=>x.Id==productAddToCard.Id)>0)
+                    {
+                        shoppingCard.FirstOrDefault(x => x.Id == productAddToCard.Id).Quantity++;
+                    }
+                    else
+                    {
+                        productAddToCard.Quantity = 1;
+                        shoppingCard.Add(productAddToCard);
+
+                    }
+                    //Önemli     
+                    //Session a bu liste atanmalı.
+                    Session["ShoppingCard"] = shoppingCard;
+                    TempData["AddToCardSucsess"] = "Ürün sepete eklendi";
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    TempData["AddToCardFailed"] = "ürün eklemesi başarısız oldu.Lütfen tekrar deneyiniz";
+                    //ex loglama yapılacak id düzgün gelmedi
+                    return RedirectToAction("Index", "Home");
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                //ex loglanacak
+                TempData["AddToCardFailed"] = "ürün eklemesi başarısız oldu.Lütfen tekrar deneyiniz";
+                return RedirectToAction("Index", "Home");
+            }
         }
     }
 }
---
+++ ECommerceLiteProject/ECommerceLiteUI/Models/CardViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ECommerceLiteUI.Models
+{
+    public class CardViewModel
+    {
+        public int Id { get; set; }
+
+        public DateTime RegisterDate { get; set; }
+
+        [Required]
+        [StringLength(maximumLength: 100, MinimumLength = 2, ErrorMessage = "Ürün adı 2 ile 100 karakter aralığında olmalıdır!")]
+        [Display(Name = "Ürün Adı")]
+        public string ProductName { get; set; }
+
+        [Required]
+        [StringLength(maximumLength: 500, ErrorMessage = "Ürün açıklaması en fazla 500 karakter olmalıdır!")]
+        [Display(Name = "Ürün Açıklaması")]
+        public string Description { get; set; }
+        [Required]
+        [Display(Name = "Ürün Kodu")]
+        [StringLength(maximumLength: 8, MinimumLength = 8, ErrorMessage = "Ürün kodu en fazla 8 karakter olmalıdır!")]
+        
+        public string ProductCode { get; set; }
+        [Required]
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public double Discount { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
